2e180d98d3cb32d12dfc960567f4370596bc4a2b
螺丝钉 <32859363+gyh03@users.noreply.github.com>
2017-11-11 20:32:43 +0800

    test

---
+++ src/main/java/com/gyh/myZookeeper/zookeeper/cluster/Client2.java
@@ -0,0 +1,12 @@
+package com.gyh.myZookeeper.zookeeper.cluster;
+
+import com.gyh.myZookeeper.zookeeper.cluster.ZKWatcher;
+
+public class Client2 {
+
+	public static void main(String[] args) throws Exception{
+		
+		ZKWatcher myWatcher = new ZKWatcher();
+		Thread.sleep(100000000);
+	}
+}
